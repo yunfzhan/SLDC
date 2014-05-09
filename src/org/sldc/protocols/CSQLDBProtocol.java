bf424b48986d54451af2ddc20bb8c6cef293e02f
yunfzhan <yfzhang9614@yahoo.com>
2014-05-08 22:08:56 -0700

    Add Ftp and Database protocol files.

---
+++ src/org/sldc/protocols/CSQLDBProtocol.java
@@ -0,0 +1,15 @@
+package org.sldc.protocols;
+
+import org.sldc.assist.CSQLChunkDataIntf;
+import org.sldc.assist.CSQLProtocol;
+import org.sldc.exception.ProtocolException;
+
+public class CSQLDBProtocol implements CSQLProtocol {
+
+	@Override
+	public CSQLChunkDataIntf Retrieve() throws ProtocolException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
