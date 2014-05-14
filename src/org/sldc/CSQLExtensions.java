9fb99cfd010eaa67f9173bdd605f022af94d4224
yunfzhan <yfzhang9614@yahoo.com>
2014-05-14 11:30:41 -0700

    Dynamically load class extension

--- src/org/sldc/CSQLExtensions.java
+++ src/org/sldc/CSQLExtensions.java
@@ -2,6 +2,8 @@ package org.sldc;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 import org.sldc.assist.CSQLBuildIns;
 
@@ -16,19 +18,21 @@ public class CSQLExtensions implements FilenameFilter, CSQLSaveInterface {
 	
 	public static CSQLSaveInterface getExtSaveClass() {
 		// Get the class path, to test if it works in jar.
-		String path = CSQLExtensions.class.getProtectionDomain().getCodeSource().getLocation()+extensionSave.replace(".", File.separator);
-		path = path.substring(path.indexOf(':')+1);
-		File f = new File(path);
+		String root = CSQLExtensions.class.getProtectionDomain().getCodeSource().getLocation().toString();// package root
+		String path = root+extensionSave.replace(".", File.separator); // directory where extensions are lying.
+		File f = new File(path.substring(path.indexOf(':')+1));
 		String[] names = f.list(new CSQLExtensions());
 		try{
+			URLClassLoader ucl = new URLClassLoader(new URL[]{new URL(root)});
 			Class<? extends CSQLSaveInterface> csi = null;
 			/*
 			 * Search the first class that derives from CSQLSaveInterface and initialize it.
 			 */
 			for(String name : names)
 				try {
-					name = extensionSave+name;
-					Class<?> clazz = Class.forName(name);
+					// full class name
+					name = extensionSave+name.substring(0,name.indexOf(".class"));
+					Class<?> clazz = ucl.loadClass(name);
 					csi = clazz.asSubclass(CSQLSaveInterface.class);
 				}catch(ClassCastException ex){
 					continue;
