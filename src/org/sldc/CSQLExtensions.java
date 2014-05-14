87913ed8cb002847a1e07f79ce8be32456413343
yunfzhan <yfzhang9614@yahoo.com>
2014-05-14 12:18:55 -0700

    Instance persists

--- src/org/sldc/CSQLExtensions.java
+++ src/org/sldc/CSQLExtensions.java
@@ -16,31 +16,37 @@ public class CSQLExtensions implements FilenameFilter, CSQLSaveInterface {
 	private static final String extensionRoot = "org.sldc.extensions";
 	private static final String extensionSave = extensionRoot+".save.";
 	
+	private static CSQLSaveInterface _instance = null;
+	
 	public static CSQLSaveInterface getExtSaveClass() {
-		// Get the class path, to test if it works in jar.
-		String root = CSQLExtensions.class.getProtectionDomain().getCodeSource().getLocation().toString();// package root
-		String path = root+extensionSave.replace(".", File.separator); // directory where extensions are lying.
-		File f = new File(path.substring(path.indexOf(':')+1));
-		String[] names = f.list(new CSQLExtensions());
-		try{
-			URLClassLoader ucl = new URLClassLoader(new URL[]{new URL(root)});
-			Class<? extends CSQLSaveInterface> csi = null;
-			/*
-			 * Search the first class that derives from CSQLSaveInterface and initialize it.
-			 */
-			for(String name : names)
-				try {
-					// full class name
-					name = extensionSave+name.substring(0,name.indexOf(".class"));
-					Class<?> clazz = ucl.loadClass(name);
-					csi = clazz.asSubclass(CSQLSaveInterface.class);
-				}catch(ClassCastException ex){
-					continue;
-				}
-			return csi.newInstance();
-		}catch (Exception e) {
-			return new CSQLExtensions();
+		if(_instance==null)
+		{
+			// Get the class path, to test if it works in jar.
+			String root = CSQLExtensions.class.getProtectionDomain().getCodeSource().getLocation().toString();// package root
+			String path = root+extensionSave.replace(".", File.separator); // directory where extensions are lying.
+			File f = new File(path.substring(path.indexOf(':')+1));
+			String[] names = f.list(new CSQLExtensions());
+			try{
+				URLClassLoader ucl = new URLClassLoader(new URL[]{new URL(root)});
+				Class<? extends CSQLSaveInterface> csi = null;
+				/*
+				 * Search the first class that derives from CSQLSaveInterface and initialize it.
+				 */
+				for(String name : names)
+					try {
+						// full class name
+						name = extensionSave+name.substring(0,name.indexOf(".class"));
+						Class<?> clazz = ucl.loadClass(name);
+						csi = clazz.asSubclass(CSQLSaveInterface.class);
+					}catch(ClassCastException ex){
+						continue;
+					}
+				_instance = csi.newInstance();
+			}catch (Exception e) {
+				_instance = new CSQLExtensions();
+			}
 		}
+		return _instance;
 	}
 
 	@Override
