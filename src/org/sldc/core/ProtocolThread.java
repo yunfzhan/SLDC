efe951482026778adfa75747d4b350e579913ca2
yunfzhan <yfzhang9614@yahoo.com>
2014-06-03 17:49:00 -0700

    Fix a bug parsing protocol node.

--- src/org/sldc/core/ProtocolThread.java
+++ src/org/sldc/core/ProtocolThread.java
@@ -4,6 +4,7 @@ import java.util.concurrent.Callable;
 
 import org.antlr.v4.runtime.misc.NotNull;
 import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.TerminalNode;
 import org.sldc.assist.CSQLUtils;
 import org.sldc.assist.multitypes.ProtocolsHelper;
 import org.sldc.csql.cSQLParser;
@@ -30,7 +31,8 @@ public class ProtocolThread implements Callable<Object[]> {
 	
 	@Override
 	public Object[] call() throws Exception {
-		key = currentNode.Identifier(1) != null?currentNode.Identifier(1).getText():currentNode;
+		TerminalNode node = (currentNode.protocol()==null)?currentNode.Identifier(1):currentNode.Identifier(0);
+		key = (node!= null)?node.getText():currentNode;
 		value = CSQLUtils.isString(key)?runner.getScope().getVarValue((String) key):runner.getScope().getVarValue((ParseTree)key);
 		if(!(value instanceof SLDCException))
 			return new Object[]{key, value};
@@ -40,7 +42,7 @@ public class ProtocolThread implements Callable<Object[]> {
 			Scope scope = new Scope(runner.getScope());
 			scope.setInput(selectExpr.condition());
 			
-			Object addr=(currentNode.Identifier(0)!=null)?runner.visit(currentNode.Identifier(0)):currentNode.protocol().getText();
+			Object addr=(currentNode.protocol()==null)?runner.visit(currentNode.Identifier(0)):currentNode.protocol().getText();
 			value = ProtocolsHelper.Retrieve(addr, scope);
 		} catch (SLDCException e) {
 			value = e;
