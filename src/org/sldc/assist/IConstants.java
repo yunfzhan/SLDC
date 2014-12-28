bf29a2d0c15a424cbc747c902ecb8eea952ae9a4
yunfzhan <yfzhang9614@yahoo.com>
2014-12-27 20:27:35 -0800

    An interface for constants

---
+++ src/org/sldc/assist/IConstants.java
@@ -0,0 +1,8 @@
+package org.sldc.assist;
+
+public interface IConstants {
+	public static final int HTML_TAG = 1;
+	public static final int HTML_ATTR = 2;
+	public static final int HTML_INNER_TEXT = 3;
+	public static final int HTML_INNER_HTML = 4;
+}
